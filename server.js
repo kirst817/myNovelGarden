7a85ac83f66371d7186f570b891d2ec2c3bebb23
Kirsten <kirstenwedde@gmail.com>
2016-08-12 14:26:04 -0600

    updated server.js

--- server.js
+++ server.js
@@ -1,2 +1,9 @@
+// var express = require('express');
+// express().use(express.static(__dirname)).listen(3000);
+
 var express = require('express');
-express().use(express.static(__dirname)).listen(3000);
+var app = express();
+
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 3000);
